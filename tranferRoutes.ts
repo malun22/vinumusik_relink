9263ffeefeecb47a0c080266764fdbb36c7d0f7e
malun22 <malun22@web.de>
2023-11-13 20:08:31 +0100

    Added NatureParadise

---
+++ tranferRoutes.ts
@@ -0,0 +1,8 @@
+interface TransferRoutes {
+  [key: string]: string;
+}
+
+export const transferRoutes: TransferRoutes = {
+  "SantoriniMod.zip": "brzgAMlsSE20mO5qBQVLlw",
+  "NatureParadise.zip": "a1fO3T5A_kCUQYdnaAywyQ",
+};
